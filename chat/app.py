d1385ade69c01f231df9950bddfc3606e367197c
Matias Schimuneck <schimuneck.matias@gmail.com>
2024-06-18 12:43:27 +0100

    App update for AWS Lambda.

--- chat/app.py
+++ chat/app.py
@@ -1,30 +1,20 @@
 import os
-import openai
 from flask import Flask, request, jsonify
-from aws_lambda_powertools.logging import correlation_paths
-from aws_lambda_powertools.middleware_factory import lambda_handler_decorator
-from aws_lambda_powertools.logging import Logger
-from aws_lambda_wsgi import response as awslambda_wsgi_response
+import openai
+import awsgi
 
 app = Flask(__name__)
 
-OPENAI_ORGANIZATION_ID = os.getenv('OPENAI_ORGANIZATION_ID')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-
-openai.api_key = OPENAI_API_KEY
-openai.organization = OPENAI_ORGANIZATION_ID
-
-logger = Logger()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    data = request.get_json()
-    user_input = data.get('user_input')
+    user_input = request.json.get('user_input')
     if not user_input:
-        return jsonify({'error': 'User input is required'}), 400
+        return jsonify({"error": "user_input is required"}), 400
 
     messages = [
-        {"role": "system", "content": "You are a customer support chat from Imediato Náutica, an online e-commerce store for marine products. You only answer product-related questions."},
+        {"role": "system", "content": "You are a customer support chat from Imetiato Nautica, an online e-commerce store for marine products. You only answer product-related questions."},
         {"role": "user", "content": user_input}
     ]
 
@@ -33,16 +23,14 @@ def chat():
             model="gpt-3.5-turbo",
             messages=messages
         )
-        assistant_response = response.choices[0].message['content']
-        return jsonify({'response': assistant_response})
-
+        assistant_response = response['choices'][0]['message']['content']
+        return jsonify({"response": assistant_response}), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({"error": str(e)}), 500
 
-@lambda_handler_decorator(logger=logger.inject_lambda_context(correlation_paths.API_GATEWAY_REST))
 def lambda_handler(event, context):
-    return awslambda_wsgi_response(app, event, context)
+    return awsgi.response(app, event, context)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(debug=True)
 
