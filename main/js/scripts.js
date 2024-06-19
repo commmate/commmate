99e985000803620235483ecc46a5344c6d008fbf
Matias Schimuneck <schimuneck.matias@gmail.com>
2024-06-19 12:50:44 +0100

    Main site page update and new deployment for AWS s3

--- main/js/scripts.js
+++ main/js/scripts.js
@@ -3,18 +3,3 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
 */
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector("head").innerHTML += data;
-            document.body.insertAdjacentHTML('afterbegin', data);
-        });
-
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML('beforeend', data);
-        });
-});
-
