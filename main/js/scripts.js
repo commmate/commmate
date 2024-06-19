71f5ec289a678ac90636cd2b580799acb0cb3d96
Matias Schimuneck <schimuneck.matias@gmail.com>
2024-06-19 13:20:15 +0100

    Add chat deploy workflow to AWS Amplify

--- main/js/scripts.js
+++
@@ -1,5 +0,0 @@
-/*!
-* Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
-*/
